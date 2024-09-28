6228cf3cab9b955a802b451898ce803f61e1d128
m0xn <moon0xcoder@gmail.com>
2024-09-28 10:56:35 +0200

    Added workaround for termux 'system' method perm issue

--- build.c
+++ build.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <stdbool.h>
 #include <dirent.h>
@@ -9,15 +10,24 @@
 
 int main(int argc, char **argv)
 {
-	DIR *target_dir = opendir(BUILD_PATH);
-	if (!target_dir) {
-		printf("[INFO]: "BUILD_PATH" dir not found, making dir...\n");
-		system("mkdir "BUILD_PATH);
-	}
 	bool debug = argv[1] != NULL && strcmp(argv[1], "debug") == 0;
 	char *build_cmd = debug 
 		? "clang -g "SRC_PATH"/main.c -o "BUILD_PATH"/main"
 		: "clang "SRC_PATH"/main.c -o "BUILD_PATH"/main";
+
+	DIR *target_dir = opendir(BUILD_PATH);
+	if (!target_dir) {
+		printf("[INFO]: "BUILD_PATH" dir not found, making dir...\n");
+		int status = system("mkdir "BUILD_PATH);
+		if (status != 0) {
+			printf("[INFO]: Failed to create "BUILD_PATH" dir, trying another method...");
+			char *compund_cmd = "mkdir "BUILD_PATH;
+			strcat(compund_cmd, "&& ");
+			strcat(compund_cmd, build_cmd);
+			execl("/bin/sh", "sh", "-c", compund_cmd, (char*)NULL);
+		}
+	}
+
 	printf("[INFO]: %s\n", build_cmd);
 	system(build_cmd);
 	return 0;
