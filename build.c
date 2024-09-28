9d3a4a82df50245a0ad1949fb95cd3e3caf290c3
m0xn <moon0xcoder@gmail.com>
2024-09-28 09:54:09 +0200

    Fixed optional 'debug' target

--- build.c
+++ build.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define SRC_PATH "src/"
 #define BUILD_PATH "target/"
 
 int main(int argc, char **argv)
 {
-	char *build_cmd = strcmp(argv[1], "debug") == 0
+	bool debug = argv[1] != NULL && strcmp(argv[1], "debug") == 0;
+	char *build_cmd = debug 
 		? "clang -g "SRC_PATH"main.c -o "BUILD_PATH"main"
 		: "clang "SRC_PATH"main.c -o "BUILD_PATH"main";
 	printf("[INFO]: %s\n", build_cmd);
