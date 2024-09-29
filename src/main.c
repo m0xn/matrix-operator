4b15ef5cf0ffb5b34a2130a41b4c7d85fe370e2a
m0xn <moon0xcoder@gmail.com>
2024-09-29 21:42:53 +0200

    Modified main file to test functions

--- src/main.c
+++ src/main.c
@@ -2,63 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <math.h>
 #include "../lib/matrix.h"
 
 #define MAX_CHAR_COUNT 180
 #define MAX_ARRAY_SIZE 25 // Support for 5x5 matrix for now 
 
-void rm_whitespace(char *src)
-{
-	char *tmp = (char*)malloc(strlen(src) + 1);
-	char *rdr = src, *out = tmp;
-	while (*rdr != '\0') {
-		if (*rdr == ' ')
-			while(*(++rdr) == ' ');
-		*(out++) = *(rdr++);
-	}
-	strcpy(src, tmp);
-	free(tmp);
-}
-
-void parse_matrix(int *elements, int *rows, int *cols, char *raw_input, const char* delimiter) {
-	char *row = strtok(raw_input, delimiter);
-	char *endptr;
-	int i = 0;
-	while (row) {
-		(*rows)++;
-		while (*row != '\0') {
-			elements[i++] = strtol(row, &endptr, 10);
-			row += *endptr == ',' || *endptr == '\n' ? endptr - row + 1 : endptr - row; 
-		}
-		row = strtok(NULL, delimiter);
-	}
-	*cols = i / *rows;
-}
-
 int main()
 {
-	char *input = (char*)malloc(MAX_CHAR_COUNT);
-	int *elements = (int*)malloc(sizeof(int)*MAX_ARRAY_SIZE);
-	size_t max_input_size = MAX_CHAR_COUNT;
-	int rows = 0, cols = 0;
+	Matrix m, n, j;
 
-	printf("Introduce your matrix (elements are separated by ',' and rows by ';'):\n");
-	getline(&input, &max_input_size, stdin);
-	rm_whitespace(input);
-	parse_matrix(elements, &rows, &cols, input, ";");
+	fill_matrix(&m, 3, 2, 10, INCREMENT, 2);
+	fill_matrix(&n, 2, 3, 10, DECREMENT, 2);
 
-	Matrix m;
-	init_matrix(&m, rows, cols, elements);
-	/*print_matrix(m);*/
-
-	/*Matrix i;*/
-	/*int order;*/
-	/*printf("Introduce the order of the identity matrix: ");*/
-	/*scanf("%d", &order);*/
-	/*identity_matrix(&i, order);*/
-	triangulate_matrix(&m, UPPER);
+	printf("M = \n");
 	print_matrix(m);
-	/*bool output = symmetric_matrix(m);*/
-	/*printf("The matrix is: %s\n", output ? "Symmetric" : "Non-symmetric");*/
+	printf("\n");
+
+	printf("N = \n");
+	print_matrix(n);
+	printf("\n");
+
+	printf("N * M = \n");
+	j = mult_matrices(n, m);
+	print_matrix(j);
+	printf("\n");
 	return 0;
 }
